f30fddbb25880f8d1fbae9f8b8dd06346da77271
ethmur <etmuop@gmail.com>
2016-11-08 15:11:56 -0500

    Update droneapi.py

--- python/droneapi.py
+++ python/droneapi.py
@@ -88,16 +88,21 @@ class  DroneAPI:
     @image: filepath of image to post, image should already be geotagged and timetagged
     returns True if post was succesful, False otherwise
     '''
-    #should rename this at some point
     def postImage(self, image):
         if self.server_url  is None:
             raise DroneAPICallError('getAccess','server url specified')
-        headers = {'Content-Type':'application/json; charset=UTF-8'}
+            
+        # put token into the header, im not sure if i did that correctly
+        headers = {'Content-Type':'application/json; charset=UTF-8', 'Authorization JWT': self.token}
         im = open(image, "rb")
         data    = {'image': im.read()}
         im.close()
         endpoint = self.server_url +'/drone/postimage'
+        
+        #send the post request
         resp = requests.post(endpoint,headers=headers,data=json.dumps(data))
+        
+        #check the response code to determine if image was succesfully posted
         if resp.status_code == 400:
             self.postAccess(self.username, self.password)
             return False
