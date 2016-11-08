7e073ec16695557664bce4573dd85c4340de530d
ethmur <etmuop@gmail.com>
2016-11-08 15:24:54 -0500

    Create imagepoller.py

---
+++ python/imagepoller.py
@@ -0,0 +1,38 @@
+import os
+
+'''
+This class is responsible for keeping track of the filepath of the next image to be posted to the ground station and 
+whether or not it is ready to be posted
+'''
+
+class ImagePoller(object):
+    
+    '''
+        Instance variables:
+          1) next_image_number: The number of the next image to be posted. This number gets incremented every time an image is succesfully
+                    posted. The default starting value is 1, but this can be overidden by specifying a different value in the constructor.
+          2) image_poll_directory: The path to the directory where images will be saved. This value remains constant for the entire duration
+                    of flight but may change on different days (assuming we create different directories for every fly day)
+          3) next_image_filepath: the full filepath of the next image to be posted. This value changes every time an image is succesfully
+                    posted. This filepath is computed by concatinating image_poll_directory, next_image_value, and some other constant literals
+    '''                
+          
+    def __init__(self, next_image_number=1, image_poll_directory):
+        self.next_image_number = next_image_number
+        self.image_poll_directory = image_poll_directory
+        self.next_image_filepath = image_poll_directory + "/capt" + str(next_image_number) + ".jpeg"
+        
+        
+        
+    # returns True if the next image is ready to be posted, returns False otherwise
+    def next_image_isready(self):
+        return os.path.isfile(next_image_filepath)
+        
+    # exposes next_image_filepath
+    def get_next_image_filepath(self):
+        return current_image_filepath
+    
+    # incrememnts current_image_number and recomputes current_image_filepath
+    def increment(self):
+        current_image_number += 1
+        current_image_filepath = image_poll_directory + "/capt" + str(current_image_number) + ".jpeg"
