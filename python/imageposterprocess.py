cdfe14fc0c562fa0e1d81a1ad305b9ee40a85c1e
ethmur <etmuop@gmail.com>
2016-11-22 22:29:26 -0500

    Create imageposterprocess.py

---
+++ python/imageposterprocess.py
@@ -0,0 +1,38 @@
+'''
+This file is the main process that is responsible for polling for new images&telemetry and posting them when they become available.
+This file uses an ImagePoller object to poll and a DroneAPI object to send the http post request
+'''
+
+import sys
+from imagepoller import ImagePoller
+from droneapi import DroneAPI
+import time
+
+# read all the command line inputs
+if len(sys.argv) != 8:
+    print("Error - Incorrect # of command line arguments passed to imageposterprocess.py. Number of arguments you passed in: " + str(len(sys.argv)-1) + ", number of arguments you need to pass in: 7")
+    sys.exit(1)
+next_image_number = argv[1]
+image_poll_directory = argv[2]
+server_url = argv[3]
+username = argv[4]
+password = argv[5]
+access_token_as_text = argv[6]
+sleep_time = argv[7]
+
+
+# initialize the ImagePoller and DroneAPI objects
+image_poller = ImagePoller(next_image_number, image_poll_directory)
+drone_api = DroneAPI(server_url, username, password)
+drone_api.setAccessToken(access_token_as_text)
+
+
+while (True):
+    if image_poller.next_image_isready():
+        success = drone_api.postImage(image_poller.get_next_image_filepath(), image_poller.get_next_telemetry_filepath())
+        if success == True:
+            print("Succesfully posted " + image_poller.get_next_image_filepath())
+        else:
+            print("Unsuccesfully posted " + image_poller.get_next_image_filepath())
+    else:
+        time.sleep(sleep_time)
