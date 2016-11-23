b117b3cd98aede4f24c148ea45895da9e3e4cb3e
ethmur <etmuop@gmail.com>
2016-11-22 21:10:25 -0500

    Update imagepoller.py

--- python/imagepoller.py
+++ python/imagepoller.py
@@ -1,8 +1,8 @@
 import os
 
 '''
-This class is responsible for keeping track of the filepath of the next image to be posted to the ground station and 
-whether or not it is ready to be posted
+This class is responsible for keeping track of the filepath of the next image and telemetry to be posted to the ground station and 
+whether or not they are ready to be posted
 '''
 
 class ImagePoller(object):
@@ -15,24 +15,43 @@ class ImagePoller(object):
                     of flight but may change on different days (assuming we create different directories for every fly day)
           3) next_image_filepath: the full filepath of the next image to be posted. This value changes every time an image is succesfully
                     posted. This filepath is computed by concatinating image_poll_directory, next_image_value, and some other constant literals
+          4) next_telemetry_filepath: the full filepath of the next telemetry file to be posted along with the image. This value changes every
+                    time an image is succesfully posted. This filepath is computed by concatinating image_poll_directory, next_image_value, 
+                    and some other constant literals
     '''                
           
+    # Constructor, takes next_image_number (default=1) and image_poll_directory as input
     def __init__(self, next_image_number=1, image_poll_directory):
         self.next_image_number = next_image_number
         self.image_poll_directory = image_poll_directory
-        self.next_image_filepath = image_poll_directory + "/capt" + str(next_image_number) + ".jpeg"
+        # if the image poll directory does not end in a file path seperator, append one
+        if (self.image_poll_directory[-1] != '/' and self.image_poll_directory != '\\'):
+            self.image_poll_directory += '/'
+        self.next_image_filepath = self.image_poll_directory + "capt" + str(self.next_image_number) + ".jpeg"
+        self.next_telemetry_filepath = self.image_poll_directory + "telem" + str(self.next_image_number) + ".txt"
         
         
         
     # returns True if the next image is ready to be posted, returns False otherwise
     def next_image_isready(self):
-        return os.path.isfile(next_image_filepath)
+        if !os.path.isfile(self.next_image_filepath) or !os.path.isfile(self.next_telemetry_filepath):
+            return False
+        # if the telemetry file exists but is empty, just skip over it
+        if os.path.getsize(self.next_telemetry_filepath) == 0:
+            self.increment()
+            return False
+        return True
         
     # exposes next_image_filepath
     def get_next_image_filepath(self):
-        return current_image_filepath
+        return self.next_image_filepath
     
-    # incrememnts current_image_number and recomputes current_image_filepath
+    # exposes next_telemetry_filepath
+    def get_next_telemetry_filepath(self):
+        return self.next_image_filepath
+    
+    # incrememnts next_image_number and recomputes next_image_filepath
     def increment(self):
-        current_image_number += 1
-        current_image_filepath = image_poll_directory + "/capt" + str(current_image_number) + ".jpeg"
+        self.current_image_number += 1
+        self.next_image_filepath = self.image_poll_directory + "capt" + str(self.current_image_number) + ".jpeg"
+        self.next_telemetry_filepath = self.image_poll_directory + "telem" + str(self.next_image_number) + ".txt"
