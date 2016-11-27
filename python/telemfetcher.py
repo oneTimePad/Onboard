20eea6427680f72f86773219b857eb6ce6e33100
Dylan Herman <dah242@scarletmail.rutgers.edu>
2016-11-27 21:47:08 +0000

    added fetching of telemetry and telemtry receiver to telemFetcher

--- python/telemfetcher.py
+++ python/telemfetcher.py
@@ -2,6 +2,7 @@
 
 import dronekit
 import serial
+import multiprocessing
 
 
 
@@ -30,6 +31,7 @@ class TelemFetcher(object):
             self.expected_msg = arduino_msg
             self.image_id = 0
             self.storage_dir = storage_template
+            self.telem_queue = None
 
     def connect_mav_server(self,wait_ready = True):
 
@@ -45,27 +47,72 @@ class TelemFetcher(object):
             raise e
 
 
-    def fetch_telemtry(self,timeout):
+    def fetch_telemtry(self):
         """
         fetch telemetry from mav and return as a dictionary
-        ---will implement
         """
-        pass
+        
+        telem = dict()
+        telem["lat"] = float(self.drone.location.global_frame.lat)
+        telem["lon"] = float(self.drone.location.global_frame.lon)
+        telem["alt"] = float(self.drone.location.global_relative_frame.alt)
+        telem["groundcourse"] = float(self.drone.heading)
+        telem["pitch"] = float(self.drone.attitude.pitch)
+        telem["yaw"] = float(self.drone.atttude.yaw)
+        telem["roll"] = float(self.drone.attitude.roll)
 
-    def start_serial_listener(self,device_port,baud=9600,time_between_images=0.5):
+        return telem
+
+
+    def telemtry_receiver(self,telem_queue):
+
+        """
+        polls for next telemetry from telem_queue and writes it to a file
+
+        telem_queue:= multiprocessing.Queue where telemtry is enqueued`
+
+        """
+        while True:
+            self.image_id+=1
+            with open("".join((storage_dir,str(self.image_id),".telem")),"w")) as f:
+                          f.write(str(telem_queue.get()))
+
+    def start_telemtry_receiver(self):
+
+        """
+            starts up telemetry receiver process which pulls telemtry from a
+            queue and writes to a file with same name as associated image with
+            .telem
+        """
+
+        self.telem_queue = multiprocessing.Queue()
+        receiver = Process(target =
+                           self.telemtry_receiver,args=(self.telem_queue,))
+        receiver.daemon  = True # don't wait for it to die
+        receiver.start()
+
+
+
+    def start_serial_listener(self,device_port,baud=9600):
 
         """
         open serial device connection and wait for message from the arduino
-        saying to fetch. write telemtry information to file
+        saying to fetch. puts telemtry in queue to be written to file
+
+        device_port := com [windows] or device file [linux] where arduino
+        serial is located
+
+        baud := serial communication rate, default is 9600
 
         """
+
+        if self.telem_queue  is None:
+            raise Exception("Please call start telemetry reciever first")
 
         serial_listener = serial.Serial(device_port,baud,0)
 
         while True:
             arduino_msg = serial_listener.read()
             if arduino_msg == self.expected_msg:
-                self.image_id+=1
-                telemtry = self.fetch_telemtry(time_between_images/2)
-                with open("".join((storage_dir,str(self.image_id),".txt")),"w")) as f:
-                          f.write(str(telemtry))
+                self.telem_queue.put(
+                    self.fetch_telemtry())
