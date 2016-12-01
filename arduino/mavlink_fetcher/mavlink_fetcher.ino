d6092b7d7d321b1eb936707df7c9570d4aff4aff
Dylan Herman <dah242@scarletmail.rutgers.edu>
2016-12-01 12:43:06 +0000

    added mavlink_fetcher, fetches mavlink on camera strobe signal

---
+++ arduino/mavlink_fetcher/mavlink_fetcher.ino
@@ -0,0 +1,48 @@
+#include <Mavlink.h>
+
+const unsigned TRIGGER_PIN = 2;
+volatile bool trigger_state = 0;
+Mavlink mav(Serial1);
+
+void setup() {
+	Serial.begin(9600);
+	while(!Serial); // for arduino micro
+  Serial1.begin(57600);
+    //attachInterrupt(digitalPinToInterrupt(TRIGGER_PIN),fct,FALLING);
+      pinMode(TRIGGER_PIN,INPUT_PULLUP);
+
+}
+
+volatile  int lol = 0;
+void loop() {
+/*
+ mav.read();
+       Mavlink::attributes attribs = mav.getAttribs();
+  Serial.print("   Lat: "); Serial.print(attribs.lat);
+  Serial.print("   Lon: "); Serial.print(attribs.lon);
+  Serial.print("   Rel Alt: "); Serial.print(attribs.rel_alt);
+  Serial.print("   Roll: "); Serial.print(attribs.roll);
+  Serial.print("   Pitch: "); Serial.print(attribs.pitch);
+  Serial.print("   Yaw: "); Serial.println(attribs.yaw);
+*/
+
+      mav.read();
+  bool current_state = digitalRead(TRIGGER_PIN);
+  //Serial.println(current_state);
+  if(current_state != trigger_state) {
+    if ((trigger_state = current_state)==LOW) {
+      Serial.println(lol++);
+    
+       Mavlink::attributes attribs = mav.getAttribs();
+  Serial.print("   Lat: "); Serial.print(attribs.lat);
+  Serial.print("   Lon: "); Serial.print(attribs.lon);
+  Serial.print("   Rel Alt: "); Serial.print(attribs.rel_alt);
+  Serial.print("   Roll: "); Serial.print(attribs.roll);
+  Serial.print("   Pitch: "); Serial.print(attribs.pitch);
+  Serial.print("   Yaw: "); Serial.println(attribs.yaw);
+    }
+    }
+
+  
+  
+}
